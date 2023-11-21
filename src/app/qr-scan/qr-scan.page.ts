8db2515c05a1eb979be4e8c55fce9ba98ac897fd
MUTInnovationLab <mutinnovationlab@gmail.com>
2023-11-21 14:22:04 +0200

    ff

--- src/app/qr-scan/qr-scan.page.ts
+++ src/app/qr-scan/qr-scan.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import * as QRCode from 'qrcode';
 
 @Component({
   selector: 'app-qr-scan',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./qr-scan.page.scss'],
 })
 export class QrScanPage implements OnInit {
+  qrCodeDataUrl: string = '';
+  qrCodeText: string = '';
+  qrCodeSize: number = 200;
+  scannedResult: any;
+  content_visibility = '';
+  moduleCode: string = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      if (params['moduleCode']) {
+        this.moduleCode = params['moduleCode'];
+        this.generateQRCode(); // Initial QR code generation
+        this.startQRCodeUpdate(); // Start the interval for QR code update
+      }
+    });
   }
 
+  startQRCodeUpdate() {
+    // Update the QR code every 5 seconds
+    setInterval(() => {
+      this.generateQRCode();
+      this.cdr.detectChanges(); // Trigger change detection
+    }, 5000);
+  }
+  
+
+  async generateQRCode() {
+    try {
+      this.qrCodeText = this.moduleCode + '-' + Date.now().toString();
+      this.qrCodeDataUrl = await QRCode.toDataURL(this.qrCodeText, {
+        width: this.qrCodeSize,
+        margin: 1,
+      });
+    } catch (error) {
+      console.error('Error generating QR code:', error);
+    }
+  }
+  
+  
 }
