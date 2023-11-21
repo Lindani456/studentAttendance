cb2e212961b4d21a3e63f4a54f7810f1929deaab
MUTInnovationLab <mutinnovationlab@gmail.com>
2023-11-21 13:54:42 +0200

    all

--- src/app/lecture/lecture.page.ts
+++ src/app/lecture/lecture.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule, NavController, ToastController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/compat/auth'
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { LoadingController } from '@ionic/angular';
+import { AlertController, NavParams } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-lecture',
@@ -6,10 +13,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lecture.page.scss'],
 })
 export class LecturePage implements OnInit {
+  showAddCard: boolean = false;
+  
 
-  constructor() { }
+  contact_nom: string = '';
+  contact_email: string = '';
+  contact_sujet: string = '';
+  contact_message: string = '';
+
+  moduleName: any;
+  moduleCode: any;
+  moduleLevel:any;
+  userData: any;
+  tableData: any[] = [];
+
+
+
+  constructor(private router: Router,
+    private navCtrl: NavController,
+    private loadingController: LoadingController,
+    private auth: AngularFireAuth,
+    private db: AngularFirestore,
+    private alertController: AlertController,
+    private toastController: ToastController) { }
 
   ngOnInit() {
+    this.getData();
   }
+  async addModule() {
+    const loader = await this.loadingController.create({
+      message: 'submitting...',
+      cssClass: 'custom-loader-class',
+    });
+    await loader.present();
+  
+    try {
+      await this.db.collection('lecturer').add({
+        moduleName: this.moduleName,
+        moduleCode: this.moduleCode,
+        moduleLevel: this.moduleLevel,
+      });
+  
+      // Clear the input fields
+      this.moduleName = '';
+      this.moduleCode = '';
+      this.moduleLevel = '';
+  
+      loader.dismiss();
+      alert('Information successfully saved');
+    } catch (error) {
+      loader.dismiss();
+      console.error('Error saving data:', error);
+      alert('An error occurred while saving the information.');
+    }
+  }
+  
+
+  gotoQRscan(moduleCode: string) {
+    this.router.navigate(['qr-scan'], { queryParams: { moduleCode } });
+  }
+  gotoProfile(moduleCode: string) {
+    this.router.navigate(['profile']);
+  }
+  gotoAttendies(moduleCode: string) {
+    this.router.navigate(['attendies']);
+  }
+
+  getData() {
+    this.db
+      .collection('lecturer', (ref) =>
+        ref.where('moduleName', '>', '')
+      )
+      .snapshotChanges()
+      .subscribe((data) => {
+        this.userData = data.map((d) => {
+          const id = d.payload.doc.id;
+          const docData = d.payload.doc.data() as any; // Cast docData as any type
+          return { id, ...docData };
+        });
+        console.log(this.userData);
+        this.tableData = this.userData;
+      });
+  }
+
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAohbllLwFHIPOllSp8011M7_u_zqRvZx0",
+    authDomain: "student-attendance-7453a.firebaseapp.com",
+    projectId: "student-attendance-7453a",
+    storageBucket: "student-attendance-7453a.appspot.com",
+    messagingSenderId: "172755450483",
+    appId: "1:172755450483:web:2224bb16b714f3c514c806"
+  }
 };
 
 /*
